4184af70fdc4a03a9d89fe07b191e46b4282caca
Anaghjaiswar <jaiswaranagh@gmail.com>
2024-10-28 20:41:29 +0530

    modifies contact.html and requirements.txt

--- recipe_project/recipes/forms.py
+++ recipe_project/recipes/forms.py
@@ -6,3 +6,6 @@ class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ['title', 'description', 'ingredients', 'instructions']
+
+
+# meta class : provides flexibility in selecting fields and configuring form behavior
